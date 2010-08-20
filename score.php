8927aa7d33eacedec7c0641fe2cd7d405d2ad8b1
Romain Bochet <romain@stackr.fr>
2010-08-20 20:47:45 +0200

    The script now writes into a file, result.dat.
    result.dat isn't track'd.

--- score.php
+++ score.php
@@ -1,9 +1,18 @@
 <?php
 
 /**
+* Allow $fp to be used in getCount
+*/
+global $fp;
+
+if (!($fp = fopen('result.dat', 'w'))) {
+    return;
+}
+
+/**
   Get the number of results  by using the google API
  */
-function getCount($obj) {
+function getCount($fp, $obj) {
 	$url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=".$obj;
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
@@ -16,14 +25,15 @@ function getCount($obj) {
 	$json = json_decode($body, true);
 
 	// Fetch the number of results
-	echo $obj.' '.$json['responseData']['cursor']['estimatedResultCount']."\n";
+	fprintf($fp, $obj.' '.$json['responseData']['cursor']['estimatedResultCount']."\n");
 }
+
 
 $vowels = array('a', 'e', 'i', 'o', 'u', 'y');
 
 foreach ($vowels as $iVowel) {
 	foreach ($vowels as $aVowel) {
-		getCount($iVowel.'p'.$aVowel.'d');
+		getCount($fp, $iVowel.'p'.$aVowel.'d');
 	}
 }
 
